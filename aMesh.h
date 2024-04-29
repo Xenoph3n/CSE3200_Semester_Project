a4a93099ab861288d97ebf117913b52438dfa0f6
Ricardo Narine <119977959+Xenoph3n@users.noreply.github.com>
2024-04-29 18:34:49 -0400

    Fixed Assimp stuff

---
+++ aMesh.h
@@ -0,0 +1,47 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "glad/glad.h"   
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/glm.hpp"
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include "shaderClass.h"
+
+#define MAX_BONE_INFLUENCE 4
+
+struct aVertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+    glm::vec3 Tangent;
+    glm::vec3 Bitangent;
+    int m_BoneIDs[MAX_BONE_INFLUENCE];
+    float m_Weights[MAX_BONE_INFLUENCE];
+};
+
+struct aTexture
+{
+    unsigned int id;
+    std::string type;
+    std::string path;
+};
+
+class aMesh {
+public:
+
+
+    std::vector<aVertex>       vertices;
+    std::vector<unsigned int> indices;
+    std::vector<aTexture>      textures;
+    unsigned int VAO;
+    aMesh(std::vector<aVertex> vertices, std::vector<unsigned int> indices, std::vector<aTexture> textures);
+    void Draw(Shader& shader);
+    void SetupMesh();
+};
+
+#endif
---
+++ aModel.h
@@ -0,0 +1,40 @@
+#ifndef A_MODEL_H
+#define A_MODEL_H
+
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "shaderClass.h"
+#include "aMesh.h"
+
+class aModel
+{
+public:
+
+    std::vector<aTexture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+    std::vector<aMesh> meshes;
+    std::string directory;
+    bool gammaCorrection;
+
+    aModel(std::string const& path, bool gamma);
+    void Draw(Shader& shader);
+
+private:
+    // model data
+    void loadModel(std::string const& path);
+    void processNode(aiNode* node, const aiScene* scene);
+    aMesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<aTexture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        std::string typeName);
+};
+#endif
