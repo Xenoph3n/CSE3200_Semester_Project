1f5e75f48fb3dfd2be41de5288ec45ab7c329612
shivmoh <shiveshmohamed1234@gmail.com>
2024-05-07 13:04:32 -0400

    some

--- Main.cpp
+++ Main.cpp
@@ -240,32 +240,7 @@ int main()
     
     Shadow shadow;
     shadow.createDepthMap(shadowShader);
-    // const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
-
-    // unsigned int depthMapFBO;
-    // glGenFramebuffers(1, &depthMapFBO);
-
-    // unsigned int depthMap;
-    // glGenTextures(1, &depthMap);
-    // glBindTexture(GL_TEXTURE_2D, depthMap);
-    // glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    // float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
-    // glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-    // // attach depth texture as FBO's depth buffer
-    // glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-    // glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
-    // glDrawBuffer(GL_NONE);
-    // glReadBuffer(GL_NONE);
-    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // shadowShader.Activate();
-    // shadowShader.setInt("texture_diffuse1", 0);
-    // shadowShader.setInt("shadowMap", 1);
-
+    
     glm::vec3 lightPos(-20.0f, 70.0f, 0.0f);
 
     glm::mat4 playerModel = glm::mat4(1.0f);
@@ -326,23 +301,6 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // 1. render depth of scene to texture (from light's perspective)
-        // --------------------------------------------------------------
-        // glm::mat4 lightProjection, lightView;
-        // glm::mat4 lightSpaceMatrix;
-        // float near_plane = 1.0f, far_plane = 500.0f;
-        // //lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
-        // lightProjection = glm::ortho(-200.0f, 200.0f, -200.0f, 200.0f, near_plane, far_plane);
-        // lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0, 1.0, 0.0));
-        // lightSpaceMatrix = lightProjection * lightView;
-        // // render scene from light's point of view
-        // depthShader.Activate();
-        // depthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix);
-
-        
-        // glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
-        // glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-        // glClear(GL_DEPTH_BUFFER_BIT);
-
         shadow.setUpDepthMap(lightPos, depthShader);
         
         depthShader.setMat4("model", glm::translate(model, glm::vec3(0.0f,0.0f,5.0f)));
@@ -353,7 +311,6 @@ int main()
 
         mega_cube.Draw(depthShader);
 
-        // glBindFramebuffer(GL_FRAMEBUFFER, 0);
         shadow.unBindFrameBuffer();
 
         // /* --------------------------------------  ---------------------------------------- */
