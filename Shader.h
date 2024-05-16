dbfccea55f3d07e711079e08f61175351dadb7c2
Oumotia <104033624+Oumotia@users.noreply.github.com>
2024-05-15 23:41:43 -0400

    Add files via upload
    pbr

--- Shader.h
+++ Shader.h
@@ -2,39 +2,54 @@
 #define SHADER_CLASS_H
 
 #include <glad/glad.h>
-
 #include <glm/glm.hpp>
-#include<string>
-#include<fstream>
-#include<sstream>
-#include<iostream>
-#include<cerrno>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cerrno>
 
-std::string get_file_contents(const char* filename);
-
-class Shader
-{
+class Shader {
 public:
-	// Reference ID of the Shader Program
-	GLuint ID;
-	// Constructor that build the Shader Program from 2 different shaders
-	Shader(const char* vertexFile, const char* fragmentFile);
+    // Reference ID of the Shader Program
+    GLuint ID;
 
-	// Activates the Shader Program
-	void Activate();
-	// Deletes the Shader Program
-	void Delete();
+    // Constructor that builds the Shader Program from vertex and fragment shaders
+    Shader(const char* vertexFile, const char* fragmentFile);
 
-	void setMat4(const std::string &name, const glm::mat4 &mat) const;
+    // Activates the Shader Program
+    void Activate();
 
-	void setVec4(const std::string &name, const glm::vec3 &vec) const;
+    // Deletes the Shader Program
+    void Delete();
 
-	void setVec3(const std::string &name, const glm::vec3 &vec) const;
+   
+    void setMat4(const std::string& name, const glm::mat4& mat) const;
 
-	void setFloat(const std::string &name, float value) const;
+    
+    void setVec4(const std::string& name, const glm::vec3& vec) const;
 
-	void setInt(const std::string &name, int value) const;
+    
+    void setVec3(const std::string& name, const glm::vec3& vec) const;
 
-	void printVec3(std::string label, glm::vec3 vector);
+    
+    void setFloat(const std::string& name, float value) const;
+
+    
+    void setInt(const std::string& name, int value) const;
+
+    
+    void setPBRMaterial(const glm::vec3& albedo, float metallic, float roughness, float ao) const;
+
+    
+    void setPBRTextures(/* Texture parameters */) const;
+
+private:
+    // Utility function to read a text file and output its contents as a string
+    std::string get_file_contents(const char* filename);
+
+    // Utility function to check for compilation/linking errors
+    void checkCompileErrors(GLuint shader, std::string type);
 };
+
 #endif
