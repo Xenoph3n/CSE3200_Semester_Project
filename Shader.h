b4bc32e7149b33aed546b78f624405a949d760f0
ShivMoh <shiveshmohamed1234@gmail.com>
2024-05-16 08:43:33 -0400

    Fixing things

--- Shader.h
+++ Shader.h
@@ -12,36 +12,27 @@
 class Shader {
 public:
     // Reference ID of the Shader Program
-    GLuint ID;
+	GLuint ID;
+	// Constructor that build the Shader Program from 2 different shaders
+	Shader(const char* vertexFile, const char* fragmentFile);
 
-    // Constructor that builds the Shader Program from vertex and fragment shaders
-    Shader(const char* vertexFile, const char* fragmentFile);
+	// Activates the Shader Program
+	void Activate();
+	// Deletes the Shader Program
+	void Delete();
 
-    // Activates the Shader Program
-    void Activate();
+	void setMat4(const std::string &name, const glm::mat4 &mat) const;
 
-    // Deletes the Shader Program
-    void Delete();
+	void setVec4(const std::string &name, const glm::vec3 &vec) const;
 
-   
-    void setMat4(const std::string& name, const glm::mat4& mat) const;
+	void setVec3(const std::string &name, const glm::vec3 &vec) const;
 
-    
-    void setVec4(const std::string& name, const glm::vec3& vec) const;
+	void setFloat(const std::string &name, float value) const;
 
-    
-    void setVec3(const std::string& name, const glm::vec3& vec) const;
-
-    
-    void setFloat(const std::string& name, float value) const;
-
-    
-    void setInt(const std::string& name, int value) const;
-
+	void setInt(const std::string &name, int value) const;
     
     void setPBRMaterial(const glm::vec3& albedo, float metallic, float roughness, float ao) const;
 
-    
     void setPBRTextures(/* Texture parameters */) const;
 
 private:
